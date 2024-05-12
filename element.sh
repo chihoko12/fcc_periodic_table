f3d7467257b9904dc29469838dcb940d76a5109e
Chihoko Neudeck <chihoko.ueno@gmail.com>
2024-05-12 18:26:26 +0000

    Initial commit

---
+++ element.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# input validation
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+  exit 1
+fi
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+# Determine the type of the input and create the appropriate SQL query
+if [[ $1 =~ ^[0-9]+$ ]]
+then
+  RESULT=$($PSQL "SELECT e.atomic_number, e.symbol, e.name, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius, t.type FROM elements e INNER JOIN properties p ON e.atomic_number = p.atomic_number INNER JOIN types t ON p.type_id = t.type_id WHERE e.atomic_number = $1;")
+elif [[ $1 =~ ^[a-zA-Z]+$ ]]
+then
+  RESULT=$($PSQL "SELECT e.atomic_number, e.symbol, e.name, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius, t.type FROM elements e INNER JOIN properties p ON e.atomic_number = p.atomic_number INNER JOIN types t ON p.type_id = t.type_id WHERE e.symbol ILIKE '$1' OR e.name ILIKE '$1';")
+else
+  echo "I could not find that element in the database."
+  exit 2
+fi
+
+if [[ -z $RESULT ]]
+then
+  echo "I could not find that element in the database."
+  exit 3
+fi
+
+#echo "$RESULT"
+
+# Extracting the fields from the result
+IFS='|' read -r atomic_number symbol name atomic_mass melting_point_celsius boiling_point_celsius type <<< "$RESULT" 
+
+# Output the details
+echo "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $atomic_mass amu. $name has a melting point of $melting_point_celsius celsius and a boiling point of $boiling_point_celsius celsius."
